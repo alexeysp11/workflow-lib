5591cd40dda2cad8ef7b060c6e021322b2a07ec9
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-06-16 20:36:36 +0500

    doc/docs -> html -> pdf

--- src/DocFormats/TextBased/Word/MSWordConverter.cs
+++ src/DocFormats/TextBased/Word/MSWordConverter.cs
@@ -1,7 +1,9 @@
 using System.IO; 
 using System.Linq; 
+using System.Xml.Linq;
 using DocumentFormat.OpenXml.Packaging; 
 using DocumentFormat.OpenXml.Wordprocessing; 
+using OpenXmlPowerTools;
 using Cims.WorkflowLib.Models.Text; 
 using Cims.WorkflowLib.DocFormats.TextBased; 
 
@@ -12,6 +14,9 @@ namespace Cims.WorkflowLib.DocFormats.TextBased.Word
     /// </summary>
     public class MSWordConverter : ITextBased 
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public void TextDocElementsToDocument(string foldername, string filename, System.Collections.Generic.List<TextDocElement> elements)
         {
             if (!Directory.Exists(foldername)) throw new System.Exception("Folder name does not exist"); 
@@ -35,6 +40,83 @@ namespace Cims.WorkflowLib.DocFormats.TextBased.Word
                     mainPart.Document.Save(); 
                 }
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ConvertToPdf(string foldername, string wordFilename, string pdfFilename)
+        {
+            if (!Directory.Exists(foldername)) throw new System.Exception("Folder name does not exist"); 
+            if (string.IsNullOrEmpty(wordFilename) || string.IsNullOrEmpty(pdfFilename)) throw new System.Exception("File name could not be null or empty"); 
+            if (wordFilename.Split('.').Last().ToLower() != "doc" && wordFilename.Split('.').Last().ToLower() != "docx") throw new System.Exception("Incorrect file extension"); 
+            if (pdfFilename.Split('.').Last().ToLower() != "pdf") throw new System.Exception("Incorrect file extension"); 
+
+            string wordPath = Path.Combine(foldername, wordFilename); 
+            string pdfPath = Path.Combine(foldername, pdfFilename); 
+            string htmlPath = pdfPath.Replace(".pdf", ".html"); 
+            if (!File.Exists(wordPath)) throw new System.Exception("MS Word file does not exist");
+
+            // Examples of how to use Open-Xml-PowerTools library for getting HTML: 
+            // https://github.com/OpenXmlDev/Open-Xml-PowerTools/blob/921cbccf6ecb29456eedc8d4d7834c7bf62c54c9/OpenXmlPowerTools.Tests/HtmlConverterTests.cs
+            // https://github.com/itext/i7n-pdfhtml/blob/develop/itext/itext.html2pdf/itext/html2pdf/HtmlConverter.cs
+            // Source code of a module that converts WordprocessingML to HTML:
+            // https://github.com/OpenXmlDev/Open-Xml-PowerTools/blob/921cbccf6ecb29456eedc8d4d7834c7bf62c54c9/OpenXmlPowerTools/WmlToHtmlConverter.cs
+            ConvertToHtml(new FileInfo(wordPath), new FileInfo(htmlPath));
+            iText.Html2pdf.HtmlConverter.ConvertToPdf(new FileInfo(htmlPath), new FileInfo(pdfPath)); 
+        }
+
+        private static void ConvertToHtml(FileInfo sourceDocx, FileInfo destFileName)
+        {
+            byte[] byteArray = File.ReadAllBytes(sourceDocx.FullName);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(byteArray, 0, byteArray.Length);
+                using (WordprocessingDocument wDoc = WordprocessingDocument.Open(memoryStream, true))
+                {
+                    var outputDirectory = destFileName.Directory;
+                    destFileName = new FileInfo(Path.Combine(outputDirectory.FullName, destFileName.Name));
+                    var imageDirectoryName = destFileName.FullName.Substring(0, destFileName.FullName.Length - 5) + "_files";
+                    int imageCounter = 0;
+                    var pageTitle = (string)wDoc.CoreFilePropertiesPart.GetXDocument().Descendants(DC.title).FirstOrDefault();
+                    if (pageTitle == null)
+                        pageTitle = sourceDocx.FullName;
+                    // var pageTitle = sourceDocx.FullName;
+
+                    WmlToHtmlConverterSettings settings = new WmlToHtmlConverterSettings()
+                    {
+                        PageTitle = pageTitle,
+                        FabricateCssClasses = true,
+                        CssClassPrefix = "pt-",
+                        RestrictToSupportedLanguages = false,
+                        RestrictToSupportedNumberingFormats = false
+                    };
+                    XElement html = WmlToHtmlConverter.ConvertToHtml(wDoc, settings);
+
+                    // Note: the xhtml returned by ConvertToHtmlTransform contains objects of type
+                    // XEntity.  PtOpenXmlUtil.cs define the XEntity class.  See
+                    // http://blogs.msdn.com/ericwhite/archive/2010/01/21/writing-entity-references-using-linq-to-xml.aspx
+                    // for detailed explanation.
+                    //
+                    // If you further transform the XML tree returned by ConvertToHtmlTransform, you
+                    // must do it correctly, or entities will not be serialized properly.
+
+                    var htmlString = html.ToString(SaveOptions.DisableFormatting);
+                    File.WriteAllText(destFileName.FullName, htmlString, System.Text.Encoding.UTF8);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert WordprocessingML to list of TextDocElement 
+        /// </summary>
+        private System.Collections.Generic.List<TextDocElement> WordprocessingMLToTextDocElements(string xmlContent)
+        {
+            if (string.IsNullOrEmpty(xmlContent)) throw new System.Exception("InnerXml could not be empty"); 
+
+            // 
+
+            return new System.Collections.Generic.List<TextDocElement>();
         }
     }
 }
