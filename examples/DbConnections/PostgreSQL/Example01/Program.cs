d3994a3ca64862d503fcde7ca651bd84001e3d0a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-25 23:30:38 +0500

    examples: added example for postgres

---
+++ examples/DbConnections/PostgreSQL/Example01/Program.cs
@@ -0,0 +1,20 @@
+﻿using Cims.WorkflowLib.Models.Business;
+using Cims.WorkflowLib.Models.Business.Delivery;
+using Cims.WorkflowLib.Models.Business.Products;
+using Cims.WorkflowLib.Examples.DbConnections.PostgreSQL.Example01.Resolvers;
+
+IEnumerable<Product> menuItems = new List<Product>();
+IEnumerable<ProductCategory> categories = new List<ProductCategory>();
+try
+{
+    menuItems = new MenuResolver().GetMenu();
+    categories = menuItems.Select(x => x.ProductCategory).Distinct();
+    foreach (var category in categories)
+    {
+        System.Console.WriteLine($"Name: {category.Name}; Description: {category.Description}");
+    }
+}
+catch (System.Exception ex)
+{
+    System.Console.WriteLine(ex);
+}
