bdb7a39e3487e57b368ce9e131c0e38948211fb8
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-03 19:52:36 +0500

    edited examples

--- examples/Delivering/Example02/Program.cs
+++ examples/Delivering/Example02/Program.cs
@@ -1,15 +1,24 @@
-﻿// 
-
-System.Console.WriteLine("Hello, World!");
-
+﻿// The program for initializing data regarding to settings of microservice communication (without adding data to the database).
+// 
 // Possible backend services:
 // - customer
 // - warehouse
 // - kitchen
 // - courier
+// - fileservice
+// - notifications
+// - esb proxy
+// - esb writer
+// - esb reader
 
 // Monolith.
+System.Console.WriteLine("Initializing monolith...");
+System.Console.WriteLine("Monolith initialized.");
 
 // HTTP.
+System.Console.WriteLine("Initializing HTTP...");
+System.Console.WriteLine("HTTP initialized.");
 
 // gRPC.
+System.Console.WriteLine("Initializing gRPC...");
+System.Console.WriteLine("gRPC initialized.");
--- examples/Delivering/Example02/README.md
+++ examples/Delivering/Example02/README.md
@@ -2,7 +2,7 @@
 
 [English](README.md) | [Русский](README.ru.md)
 
-The example is related to initializing data regarding settings of microservice communication (without adding data to the database).
+The example is related to initializing data regarding to settings of microservice communication (without adding data to the database).
 
 Let's say there is a requirement that communication between backend services be as flexible as possible (for example, as shown in the figure below).
 In this case, it is assumed that a common database will be used in which the details of interservice communication will be stored.
