ab1659ecf1c5089ac7a88b6b87eb9ddce640487a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-06 21:15:31 +0500

    examples: added description for ConfigResolver

---
+++ examples/ServiceInteraction/Core/Resolvers/ConfigResolver.cs
@@ -0,0 +1,9 @@
+namespace WorkflowLib.Examples.ServiceInteraction.Core.Resolvers;
+
+/// <summary>
+/// This class is responsible for interacting with configuration data 
+/// about communication between system components.
+/// </summary>
+public class ConfigResolver
+{
+}
