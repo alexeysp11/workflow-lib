fc9a178da6c25debb25d3e829852a187e803dc48
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-08 21:13:19 +0500

    models: added classes for process state machine

---
+++ src/Models/Business/Processes/BusinessProcessState.cs
@@ -0,0 +1,18 @@
+namespace Cims.WorkflowLib.Models.Business.Processes
+{
+    /// <summary>
+    /// General description of the business process state.
+    /// </summary>
+    public class BusinessProcessState : BusinessEntityWF, IBusinessEntityWF
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public BusinessProcess BusinessProcess { get; set; }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public System.DateTime DateCreated { get; set; }
+    }
+}
