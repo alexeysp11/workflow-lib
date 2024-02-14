4fac8a6d6627fcd1e3445b45d2746c923eaf396c
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-14 23:08:57 +0500

    examples: edited resolvers in ServiceInteraction

--- examples/ServiceInteraction/Core/Resolvers/ConfigResolver.cs
+++ examples/ServiceInteraction/Core/Resolvers/ConfigResolver.cs
@@ -39,6 +39,11 @@ public class ConfigResolver
         context.SaveChanges();
     }
 
+    public void CallNext()
+    {
+        // 
+    }
+
     /// <summary>
     /// 
     /// </summary>
@@ -46,7 +51,31 @@ public class ConfigResolver
     {
         using var context = new ServiceInteractionContext(_contextOptions);
 
+        var stringBuilder = new StringBuilder();
         var dtNow = System.DateTime.UtcNow;
+
+        // Backend service names.
+        var customerBackendName = "Customer backend";
+        var whBackendName = "Warehouse backend";
+        var courierBackendName = "Courier backend";
+        var kitchenBackendName = "Kitchen backend";
+        var fileserviceBackendName = "File service";
+        var backendServiceNames = new List<string>
+        {
+            customerBackendName,
+            whBackendName,
+            courierBackendName,
+            kitchenBackendName,
+            fileserviceBackendName
+        };
+
+        // Endpoint call type names.
+        var endpointCallTypes = new List<string>
+        {
+            EndpointCallType.Monolith.ToString(),
+            EndpointCallType.HTTP.ToString(),
+            EndpointCallType.gRPC.ToString()
+        };
 
         // Process.
         var process = new BusinessProcess
@@ -56,103 +85,92 @@ public class ConfigResolver
         };
 
         // Process states.
-        var customer = new BusinessProcessState
+        var customerState = new BusinessProcessState
         {
-            Name = "Customer backend",
+            Name = customerBackendName,
             BusinessProcess = process,
             DateCreated = dtNow
         };
-        var wh2kitchen = new BusinessProcessState
+        var wh2kitchenState = new BusinessProcessState
         {
-            Name = "Warehouse backend (wh2kitchen)",
+            Name = whBackendName + " (wh2kitchen)",
             BusinessProcess = process,
             DateCreated = dtNow
         }; 
-        var wh2courier = new BusinessProcessState
+        var wh2courierState = new BusinessProcessState
         {
-            Name = "Warehouse backend (wh2courier)",
+            Name = whBackendName + " (wh2courier)",
             BusinessProcess = process,
             DateCreated = dtNow
         };
-        var courier = new BusinessProcessState
+        var courierState = new BusinessProcessState
         {
-            Name = "Courier backend",
+            Name = courierBackendName,
             BusinessProcess = process,
             DateCreated = dtNow
         }; 
-        var kitchen = new BusinessProcessState
+        var kitchenState = new BusinessProcessState
         {
-            Name = "Kitchen backend",
+            Name = kitchenBackendName,
             BusinessProcess = process,
             DateCreated = dtNow
         }; 
-        var fileserivce = new BusinessProcessState
+        var fileserivceState = new BusinessProcessState
         {
-            Name = "File service",
+            Name = fileserviceBackendName,
             BusinessProcess = process,
             DateCreated = dtNow
         };
         var states = new List<BusinessProcessState>
         {
-            customer,
-            wh2kitchen,
-            wh2courier,
-            courier,
-            kitchen,
-            fileserivce
+            customerState,
+            wh2kitchenState,
+            wh2courierState,
+            courierState,
+            kitchenState,
+            fileserivceState
         };
 
         // Transitions.
+        var customer_wh2kitchen = new BusinessProcessStateTransition
+        {
+            BusinessProcess = process,
+            FromState = customerState,
+            ToState = wh2kitchenState,
+            DateCreated = dtNow
+        };
+        var wh2kitchen_kitchen = new BusinessProcessStateTransition
+        {
+            BusinessProcess = process,
+            FromState = wh2kitchenState,
+            ToState = kitchenState,
+            DateCreated = dtNow
+        };
+        var kitchen_wh2courier = new BusinessProcessStateTransition
+        {
+            BusinessProcess = process,
+            FromState = kitchenState,
+            ToState = wh2courierState,
+            DateCreated = dtNow
+        };
+        var wh2courier_courier = new BusinessProcessStateTransition
+        {
+            BusinessProcess = process,
+            FromState = wh2courierState,
+            ToState = courierState,
+            DateCreated = dtNow
+        };
         var transitions = new List<BusinessProcessStateTransition>
         {
-            new BusinessProcessStateTransition
-            {
-                BusinessProcess = process,
-                FromState = customer,
-                ToState = wh2kitchen,
-                DateCreated = dtNow
-            },
-            new BusinessProcessStateTransition
-            {
-                BusinessProcess = process,
-                FromState = wh2kitchen,
-                ToState = kitchen,
-                DateCreated = dtNow
-            },
-            new BusinessProcessStateTransition
-            {
-                BusinessProcess = process,
-                FromState = kitchen,
-                ToState = wh2courier,
-                DateCreated = dtNow
-            },
-            new BusinessProcessStateTransition
-            {
-                BusinessProcess = process,
-                FromState = wh2courier,
-                ToState = courier,
-                DateCreated = dtNow
-            }
+            customer_wh2kitchen,
+            wh2kitchen_kitchen,
+            kitchen_wh2courier,
+            wh2courier_courier
         };
 
         // Endpoint types.
-        var stringBuilder = new StringBuilder();
-        var endpointCallTypes = new List<string>
-        {
-            "HTTP",
-            "gRPC",
-            "monolith"
-        };
-        var endpointShortNames = new List<string>
-        {
-            "Customer backend",
-            "Warehouse backend",
-            "Courier backend",
-            "Kitchen backend",
-            "File service"
-        };
         var endpointTypes = new List<EndpointType>();
-        foreach (var endpointShortName in endpointShortNames)
+        foreach (var endpointShortName in backendServiceNames)
         {
             foreach (var endpointCallType in endpointCallTypes)
             {
@@ -166,15 +184,80 @@ public class ConfigResolver
         }
 
         // Endpoint calls.
-        var endpointCalls = new List<EndpointCall>
+        var endpointCalls = new List<EndpointCall>();
+        foreach (var endpointCallType in endpointCallTypes)
         {
-            // 
-        };
+            EndpointCallType callTypeEnum;
+            if (!System.Enum.TryParse(endpointCallType, out callTypeEnum))
+                throw new System.Exception("Could not convert string into endpoint call type enumeration");
+            
+            var specificEndpointTypes = endpointTypes.Where(x => x.Name.Contains(endpointCallType));
+            if (specificEndpointTypes == null || !specificEndpointTypes.Any())
+                continue;
+            
+            var customerEndpointType = specificEndpointTypes.FirstOrDefault(x => x.Name.Contains(customerBackendName));
+            var whEndpointType = specificEndpointTypes.FirstOrDefault(x => x.Name.Contains(whBackendName));
+            var courierEndpointType = specificEndpointTypes.FirstOrDefault(x => x.Name.Contains(courierBackendName));
+            var kitchenEndpointType = specificEndpointTypes.FirstOrDefault(x => x.Name.Contains(kitchenBackendName));
+            var fileserviceEndpointType = specificEndpointTypes.FirstOrDefault(x => x.Name.Contains(fileserviceBackendName));
+            
+            if (customerEndpointType == null)
+                throw new System.ArgumentNullException(nameof(customerEndpointType));
+            if (whEndpointType == null)
+                throw new System.ArgumentNullException(nameof(whEndpointType));
+            if (courierEndpointType == null)
+                throw new System.ArgumentNullException(nameof(courierEndpointType));
+            if (kitchenEndpointType == null)
+                throw new System.ArgumentNullException(nameof(kitchenEndpointType));
+            if (fileserviceEndpointType == null)
+                throw new System.ArgumentNullException(nameof(fileserviceEndpointType));
+            
+            endpointCalls.Add(new EndpointCall
+            {
+                EndpointTypeFrom = customerEndpointType,
+                EndpointTypeTo = fileserviceEndpointType,
+                EndpointCallType = callTypeEnum,
+                BusinessProcessState = customerState
+            });
+            endpointCalls.Add(new EndpointCall
+            {
+                EndpointTypeFrom = customerEndpointType,
+                EndpointTypeTo = whEndpointType,
+                EndpointCallType = callTypeEnum,
+                BusinessProcessState = customerState,
+                BusinessProcessStateTransition = customer_wh2kitchen
+            });
+            endpointCalls.Add(new EndpointCall
+            {
+                EndpointTypeFrom = whEndpointType,
+                EndpointTypeTo = kitchenEndpointType,
+                EndpointCallType = callTypeEnum,
+                BusinessProcessState = wh2kitchenState,
+                BusinessProcessStateTransition = wh2kitchen_kitchen
+            });
+            endpointCalls.Add(new EndpointCall
+            {
+                EndpointTypeFrom = kitchenEndpointType,
+                EndpointTypeTo = whEndpointType,
+                EndpointCallType = callTypeEnum,
+                BusinessProcessState = kitchenState,
+                BusinessProcessStateTransition = kitchen_wh2courier
+            });
+            endpointCalls.Add(new EndpointCall
+            {
+                EndpointTypeFrom = whEndpointType,
+                EndpointTypeTo = courierEndpointType,
+                EndpointCallType = callTypeEnum,
+                BusinessProcessState = wh2courierState,
+                BusinessProcessStateTransition = wh2courier_courier
+            });
+        }
 
         context.BusinessProcesses.Add(process);
         context.BusinessProcessStates.AddRange(states);
         context.BusinessProcessStateTransitions.AddRange(transitions);
         context.EndpointTypes.AddRange(endpointTypes);
+        context.EndpointCalls.AddRange(endpointCalls);
 
         context.SaveChanges();
     }
