4d1d21f54967c38e953f48a944bb60a92ce42e55
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-17 21:14:14 +0500

    models: added classes for the eav model

---
+++ src/Models/Business/Products/ProductAttributeType.cs
@@ -0,0 +1,13 @@
+namespace Cims.WorkflowLib.Models.Business.Products
+{
+    /// <summary>
+    /// Product attribute type.
+    /// </summary>
+    public enum ProductAttributeType
+    {
+        String,
+        Integer,
+        Float,
+        Date
+    }
+}
