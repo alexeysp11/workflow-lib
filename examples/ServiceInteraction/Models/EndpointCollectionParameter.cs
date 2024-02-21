f45612de44f52e00323737cb0b9ffed501effa95
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-21 21:00:42 +0500

    examples: added endpoint collection parameter

---
+++ examples/ServiceInteraction/Models/EndpointCollectionParameter.cs
@@ -0,0 +1,57 @@
+namespace WorkflowLib.Examples.ServiceInteraction.Models;
+
+/// <summary>
+/// Endpoint collection parameter.
+/// </summary>
+public class EndpointCollectionParameter
+{
+    /// <summary>
+    /// ID of endpoint collection parameter.
+    /// </summary>
+    public long Id { get; set; }
+
+    /// <summary>
+    /// Name of endpoint collection parameter.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Amount of active connection.
+    /// </summary>
+    public int ActiveConnectionCount { get; set; }
+
+    /// <summary>
+    /// Process time (CPU time).
+    /// </summary>
+    public int ProcessTime { get; set; }
+
+    /// <summary>
+    /// System load (CPU load).
+    /// </summary>
+    public int SystemLoad { get; set; }
+
+    /// <summary>
+    /// Memory usage.
+    /// </summary>
+    public int MemoryUsage { get; set; }
+
+    /// <summary>
+    /// Network speed.
+    /// </summary>
+    public int NetworkSpeed { get; set; }
+
+    /// <summary>
+    /// Processed requests.
+    /// </summary>
+    public int ProcessedRequests { get; set; }
+
+    /// <summary>
+    /// Response time.
+    /// </summary>
+    public System.TimeSpan ResponseTime { get; set; }
+
+    /// <summary>
+    /// Weight of the endpoint.
+    /// </summary>
+    public int EndpointWeight { get; set; }
+}
