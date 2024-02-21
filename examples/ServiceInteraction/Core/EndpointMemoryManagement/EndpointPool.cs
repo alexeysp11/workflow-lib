4ee67ff7ba61c3973e90c04d9d1711e28ab374ad
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-21 22:19:02 +0500

    examples: added EndpointPool

--- examples/ServiceInteraction/Core/EndpointMemoryManagement/EndpointPool.cs
+++ examples/ServiceInteraction/Core/EndpointMemoryManagement/EndpointPool.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 
+namespace WorkflowLib.Examples.ServiceInteraction.Core.EndpointMemoryManagement;
+
 /// <summary>
 /// Class for managing a pool of endpoints.
 /// </summary>
