93926a5f1cf4dc46f6be44de93cb0e6c0a88ed50
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-04-07 19:53:50 +0500

    examples: fixed inheritance problem in BLProcessingPipes

--- examples/ServiceInteraction/BL/BLProcessingPipes/CourierPipe.cs
+++ examples/ServiceInteraction/BL/BLProcessingPipes/CourierPipe.cs
@@ -1,3 +1,4 @@
+using WorkflowLib.Examples.ServiceInteraction.BL.Controllers;
 using WorkflowLib.Examples.ServiceInteraction.Core.ProcessingPipes;
 
 namespace WorkflowLib.Examples.ServiceInteraction.BL.BLProcessingPipes;
@@ -5,10 +6,29 @@ namespace WorkflowLib.Examples.ServiceInteraction.BL.BLProcessingPipes;
 /// <summary>
 /// Pipe which is responsible for processing the request on the courier service side.
 /// </summary>
-public class CourierPipe : BaseBLServicePipe
+public class CourierPipe : AbstractProcessingPipe
 {
+    private static IImplicitService m_service;
+
     /// <summary>
     /// Default constructor.
     /// </summary>
     public CourierPipe(System.Action<IProcessingPipeDelegateParams> function) : base(function) {}
+    
+    /// <summary>
+    /// Sets service instance.
+    /// </summary>
+    public static void SetService(IImplicitService service)
+    {
+        m_service = service;
+    }
+
+    /// <summary>
+    /// Method for performing a unit of work as part of a request processing sequence.
+    /// </summary>
+    public override void Handle(IProcessingPipeDelegateParams parameters)
+    {
+        m_service.ProcessPreviousService(parameters.WorkflowInstanceId, parameters.BusinessProcessStateTransitionId);
+        m_function(parameters);
+    }
 }
--- examples/ServiceInteraction/BL/BLProcessingPipes/CustomerPipe.cs
+++ examples/ServiceInteraction/BL/BLProcessingPipes/CustomerPipe.cs
@@ -1,3 +1,4 @@
+using WorkflowLib.Examples.ServiceInteraction.BL.Controllers;
 using WorkflowLib.Examples.ServiceInteraction.Core.ProcessingPipes;
 
 namespace WorkflowLib.Examples.ServiceInteraction.BL.BLProcessingPipes;
@@ -5,10 +6,29 @@ namespace WorkflowLib.Examples.ServiceInteraction.BL.BLProcessingPipes;
 /// <summary>
 /// Pipe which is responsible for processing the request on the customer service side.
 /// </summary>
-public class CustomerPipe : BaseBLServicePipe
+public class CustomerPipe : AbstractProcessingPipe
 {
+    private static IImplicitService m_service;
+
     /// <summary>
     /// Default constructor.
     /// </summary>
     public CustomerPipe(System.Action<IProcessingPipeDelegateParams> function) : base(function) {}
+    
+    /// <summary>
+    /// Sets service instance.
+    /// </summary>
+    public static void SetService(IImplicitService service)
+    {
+        m_service = service;
+    }
+
+    /// <summary>
+    /// Method for performing a unit of work as part of a request processing sequence.
+    /// </summary>
+    public override void Handle(IProcessingPipeDelegateParams parameters)
+    {
+        m_service.ProcessPreviousService(parameters.WorkflowInstanceId, parameters.BusinessProcessStateTransitionId);
+        m_function(parameters);
+    }
 }
