3ddace88dc06ad592c8f6528be33a7d4ff3a7e15
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-05-04 23:18:41 +0500

    examples: api methods in CustomerController

--- examples/Delivering/ServiceInteraction/Webapi/customer/Controllers/CustomerController.cs
+++ examples/Delivering/ServiceInteraction/Webapi/customer/Controllers/CustomerController.cs
@@ -1,46 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using WorkflowLib.Examples.Delivering.ServiceInteraction.Webapi.Customer;
 using WorkflowLib.Models.Business.BusinessDocuments;
+using WorkflowLib.Models.Business.Monetary;
 
-namespace WorkflowLib.Examples.Delivering.ServiceInteraction.Webapi.Customer.Controllers;
-
-/// <summary>
-/// Controller for processing customer requests.
-/// </summary>
-[ApiController]
-[Route("[controller]")]
-public class CustomerController : ControllerBase
+namespace WorkflowLib.Examples.Delivering.ServiceInteraction.Webapi.Customer.Controllers
 {
-    private readonly ILogger<CustomerController> _logger;
-    private IStartupInstance _startupInstance;
-
     /// <summary>
-    /// Default constructor.
+    /// Controller for processing customer requests.
     /// </summary>
-    public CustomerController(
-        ILogger<CustomerController> logger,
-        IStartupInstance startupInstance)
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
     {
-        _logger = logger;
-        _startupInstance = startupInstance;
-    }
+        private readonly ILogger<CustomerController> _logger;
+        private IStartupInstance _startupInstance;
 
-    /// <summary>
-    /// Start order processing.
-    /// </summary>
-    [HttpPost(Name = "MakeOrder")]
-    public string MakeOrder(InitialOrder initialOrder)
-    {
-        _startupInstance.Run();
-        return "Stab: MakeOrder";
-    }
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CustomerController(
+            ILogger<CustomerController> logger,
+            IStartupInstance startupInstance)
+        {
+            _logger = logger;
+            _startupInstance = startupInstance;
+        }
 
-    /// <summary>
-    /// Get order processing status.
-    /// </summary>
-    [HttpGet(Name = "GetOrderStatus")]
-    public string GetOrderStatus()
-    {
-        return "Stab: GetOrderStatus";
+        /// <summary>
+        /// Start order processing.
+        /// </summary>
+        [HttpPost("MakeOrder", Name = "MakeOrder")]
+        public string MakeOrder(InitialOrder initialOrder)
+        {
+            _startupInstance.Run();
+            return "Stab: MakeOrder";
+        }
+
+        /// <summary>
+        /// Pay for registered order.
+        /// </summary>
+        [HttpPost("MakePayment", Name = "MakePayment")]
+        public string MakePayment(Payment payment)
+        {
+            return "Stab: MakePayment";
+        }
+
+        /// <summary>
+        /// Get order processing status.
+        /// </summary>
+        [HttpGet("GetOrderStatus", Name = "GetOrderStatus")]
+        public string GetOrderStatus()
+        {
+            return "Stab: GetOrderStatus";
+        }
     }
 }
