38e8a400057c8900fe0eae6a5c95868f4d7670b2
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-05-05 19:31:56 +0500

    examples: deleted Core since it became a part of library

--- examples/Delivering/ServiceInteraction/Core/EsbProxies/EsbMqReader.cs
+++
@@ -1,9 +0,0 @@
-namespace WorkflowLib.Examples.Delivering.ServiceInteraction.Core.EsbProxies;
-
-/// <summary>
-/// Enables communication between components by listening to a message broker queue.
-/// </summary>
-public class EsbMqReader
-{
-    //
-}
--- examples/Delivering/ServiceInteraction/Core/EsbProxies/EsbSidecarProxy.cs
+++
@@ -1,9 +0,0 @@
-namespace WorkflowLib.Examples.Delivering.ServiceInteraction.Core.EsbProxies;
-
-/// <summary>
-/// Enables communication between components by using sidecar proxy, aka any deployed app.
-/// </summary>
-public abstract class EsbSidecarProxy
-{
-    // 
-}
