10d6841457057b1f7975adf18aae36c0cba3fc5a
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-05-05 11:38:10 +0500

    Models.Business: added new values for BusinessTaskDiscriminator

--- src/Models.Business/Processes/BusinessTaskDiscriminator.cs
+++ src/Models.Business/Processes/BusinessTaskDiscriminator.cs
@@ -7,12 +7,39 @@ namespace WorkflowLib.Models.Business.Processes
     /// </summary>
     public enum BusinessTaskDiscriminator
     {
+        /// <summary>
+        /// Request from store to the warehouse.
+        /// </summary>
         [Display(Name = "RequestStore2Wh")]
         RequestStore2Wh,
 
+        /// <summary>
+        /// Confirm delivering from store to the warehouse.
+        /// </summary>
         [Display(Name = "ConfirmStore2Wh")]
         ConfirmStore2Wh,
 
+        /// <summary>
+        /// Common delivery operation.
+        /// </summary>
+        [Display(Name = "DeliveryOperation")]
+        DeliveryOperation,
+
+        /// <summary>
+        /// Common cooking task.
+        /// </summary>
+        [Display(Name = "CookingOperation")]
+        CookingOperation,
+
+        /// <summary>
+        /// Common business task.
+        /// </summary>
+        [Display(Name = "BusinessTask")]
+        BusinessTask,
+
+        /// <summary>
+        /// Other business task.
+        /// </summary>
         [Display(Name = "Other")]
         Other
     }
