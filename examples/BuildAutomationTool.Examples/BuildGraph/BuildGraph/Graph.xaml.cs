38c78eba821ac2453d80184dc0733daae83fa7bc
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-06-23 13:24:40 +0500

    issue-10: renamed WpfApp5 to BuildGraph

---
+++ examples/BuildAutomationTool.Examples/BuildGraph/BuildGraph/Graph.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BuildGraph
+{
+    /// <summary>
+    /// Logic for Graph.xaml
+    /// </summary>
+    public partial class Graph : Window
+    {
+        public Graph()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
+
+
+    }
+}
