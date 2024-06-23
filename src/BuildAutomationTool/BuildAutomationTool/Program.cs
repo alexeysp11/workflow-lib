154b243a6692f64d76169fb6372fc7a105b6ea08
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-06-23 12:47:13 +0500

    issue-10: added src of BuildAutomationTool

---
+++ src/BuildAutomationTool/BuildAutomationTool/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using BuildAutomationTool.Managers;
+using BuildAutomationTool.Models;
+using BuildAutomationTool.Logging;
+
+namespace BuildAutomationTool;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        var app = host.Services.GetRequiredService<IStartupInstance>();
+        app.Start();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                ConfigureServices(services);
+            });
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Register application instance.
+        services.AddSingleton<IStartupInstance, StartupInstance>();
+
+        // Register modules.
+        services.AddSingleton<Logger>();
+        services.AddSingleton<CommandLineManager>();
+        services.AddSingleton<GitManager>();
+        services.AddSingleton<ProjectManager>();
+
+        // Configuration settings.
+        var appsettingsConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var appSettings = appsettingsConfig.GetSection("BuildAutomationToolSettings").Get<BuildAutomationToolSettings>();
+        services.AddSingleton(appSettings);
+    }
+}
