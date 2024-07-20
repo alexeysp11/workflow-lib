f3820fa303ee78d898e1c829bf0a848bbc742434
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-07-20 11:54:30 +0500

    issue-64: delete unnecessary spaces

--- examples/PublicTransportDevices/models/Data/DeviceSpecific/DriverConsoleInfo.cs
+++ examples/PublicTransportDevices/models/Data/DeviceSpecific/DriverConsoleInfo.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic; 
+using System.Collections.Generic;
 
 namespace WorkflowLib.Examples.PublicTransportDevices.Models.Data;
 
--- examples/PublicTransportDevices/tests/RabbitMQSender/Program.cs
+++ examples/PublicTransportDevices/tests/RabbitMQSender/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System.Threading;
 using System.Text;
-using System.Text.Json; 
+using System.Text.Json;
 using RabbitMQ.Client;
 using WorkflowLib.Examples.PublicTransportDevices.Models.Data;
 
@@ -10,7 +10,7 @@ var factory = new ConnectionFactory { HostName = "localhost" };
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-string exchangeName = "ptd_exchange"; 
+string exchangeName = "ptd_exchange";
 
 channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Direct);
 
@@ -19,9 +19,9 @@ for (int i = 0; i < 100; i++)
 {
     Thread thread = new Thread(() => 
     {
-        var maxj = 1000; 
-        var rnd = new System.Random(); 
-        var dt1 = System.DateTime.Now; 
+        var maxj = 1000;
+        var rnd = new System.Random();
+        var dt1 = System.DateTime.Now;
         for (int j = 0; j < maxj; j++)
         {
             string message = JsonSerializer.Serialize(new DeviceInfo 
@@ -34,25 +34,25 @@ for (int i = 0; i < 100; i++)
                     }, 
                     DateTimeCreated = System.DateTime.Now, 
                     SpecificData = new VehicleValidatorInfo { NumberSuccessful = 12 }
-                }); 
+                });
             var body = Encoding.UTF8.GetBytes(message);
             channel.BasicPublish(exchange: exchangeName,
                                 routingKey: "",
                                 basicProperties: null,
                                 body: body);
-            Thread.Sleep(5); 
+            Thread.Sleep(5);
             if (j == maxj - 1)
             {
-                var dt2 = System.DateTime.Now; 
-                var dif = dt2 - dt1; 
-                Console.WriteLine($"Thread '{Thread.CurrentThread.ManagedThreadId}' finished (Started: {dt1}, Executed: {dt2}, Executed in: {dif.Seconds}:{dif.Milliseconds})"); 
+                var dt2 = System.DateTime.Now;
+                var dif = dt2 - dt1;
+                Console.WriteLine($"Thread '{Thread.CurrentThread.ManagedThreadId}' finished (Started: {dt1}, Executed: {dt2}, Executed in: {dif.Seconds}:{dif.Milliseconds})");
             }
         }
-    }); 
-    thread.Start(); 
+    });
+    thread.Start();
 }
 
 // 
 Console.ReadLine();
-channel.Close(); 
-connection.Close(); 
+channel.Close();
+connection.Close();
