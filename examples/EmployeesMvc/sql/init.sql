c81af820238f0ddd0e4ccda856cdb4abb2521f28
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-08-18 10:18:12 +0500

    issue-92: added database

---
+++ examples/EmployeesMvc/sql/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE employeesmvc_test
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE employeesmvc_test
+    IS 'Test database for EmployeesMvc application';
+
