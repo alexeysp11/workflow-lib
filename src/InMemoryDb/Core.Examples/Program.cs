c9af344a53d2cc51318ec6a50843736e1accb24f
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-06 14:40:50 +0500

    issue-121: relational database class

--- src/InMemoryDb/Core.Examples/Program.cs
+++ src/InMemoryDb/Core.Examples/Program.cs
@@ -1,48 +1,23 @@
 ﻿using System.Data;
+using WorkflowLib.InMemoryDb.Core.DataStorage.Databases;
 
-var table = CreateTable("ParentTable");
-CreateColumn(table, "Id", System.Type.GetType("System.Int32"), unique: true);
+// Initialize.
+var db = new RelationalDatabase("TestDatabase");
+var table = db.CreateTable("ParentTable");
+db.CreateColumn(table, "Id", typeof(int), unique: true);
 
+// Add data.
 for (int i = 0; i <= 2; i++)
 {
     var row = table.NewRow();
     row["Id"] = i;
-    AddRow(table, row);
+    db.AddRow(table, row);
 }
 
+// Print data.
 Console.WriteLine("Values:");
 foreach (DataRow row in table.Rows)
 {
     var rowValue = row["Id"].ToString();
     Console.WriteLine($"- {rowValue}");;
-}
-
-DataTable CreateTable(string name)
-{
-    return new DataTable(name);
-}
-
-void CreateColumn(
-    DataTable table,
-    string name,
-    Type type,
-    int? maxLength = null,
-    string? caption = null,
-    bool readOnly = false,
-    bool unique = false,
-    bool autoIncrement = false)
-{
-    var column = new DataColumn();
-
-    column.ColumnName = name;
-    column.DataType = type;
-    column.ReadOnly = readOnly;
-    column.Unique = unique;
-
-    table.Columns.Add(column);
-}
-
-void AddRow(DataTable table, DataRow row)
-{
-    table.Rows.Add(row);
 }
