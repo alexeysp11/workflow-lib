9585a13b89bd7f066a8702d47ad4fa10436e2691
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-06 13:36:58 +0500

    issue-121: template for examples

---
+++ src/InMemoryDb/Core.Examples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+
+var table = new DataTable("ParentTable");
+
+var column = new DataColumn();
+column.DataType = System.Type.GetType("System.Int32");
+column.ColumnName = "Id";
+column.ReadOnly = false;
+column.Unique = true;
+
+table.Columns.Add(column);
+
+for (int i = 0; i <= 2; i++)
+{
+    var row = table.NewRow();
+    row["Id"] = i;
+    table.Rows.Add(row);
+}
+
+Console.WriteLine("Values:");
+foreach (DataRow row in table.Rows)
+{
+    var rowValue = row["Id"].ToString();
+    Console.WriteLine($"- {rowValue}");;
+}
