e59d49c329766bd9e115914b774c74bf31ab5d9b
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-13 10:24:42 +0500

    issue-50: removed unnecessary nesting of projects

---
+++ examples/Devilering.ServiceInteraction/Webapi.Customer/Controllers/CustomerController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using WorkflowLib.Examples.Delivering.ServiceInteraction.BL.BLProcPipes;
+using WorkflowLib.Shared.ServiceDiscoveryBpm.Routing;
+using WorkflowLib.Examples.Delivering.ServiceInteraction.Webapi.Customer;
+using WorkflowLib.Shared.Models.Business.BusinessDocuments;
+using WorkflowLib.Shared.Models.Business.Monetary;
+
+namespace WorkflowLib.Examples.Delivering.ServiceInteraction.Webapi.Customer.Controllers
+{
+    /// <summary>
+    /// Controller for processing customer requests.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private EsbControlPlane m_controlPlane;
+        private IStartupInstance _startupInstance;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public CustomerController(
+            ILogger<CustomerController> logger,
+            EsbControlPlane controlPlane,
+            IStartupInstance startupInstance)
+        {
+            _logger = logger;
+            m_controlPlane = controlPlane;
+            _startupInstance = startupInstance;
+        }
+
+        /// <summary>
+        /// Start order processing.
+        /// </summary>
+        [HttpPost("MakeOrder", Name = "MakeOrder")]
+        public string MakeOrder(InitialOrder initialOrder)
+        {
+            _startupInstance.Run();
+            
+            var parameters = new PipeDelegateParams
+            {
+                WorkflowInstanceId = 0,
+                BPStateTransitionId = 0,
+                UserId = 1,
+                InitialOrder = initialOrder
+            };
+            m_controlPlane.MoveWorkflowInstanceNext(parameters);
+
+            return "Stab: MakeOrder";
+        }
+
+        /// <summary>
+        /// Pay for registered order.
+        /// </summary>
+        [HttpPost("MakePayment", Name = "MakePayment")]
+        public string MakePayment(Payment payment)
+        {
+            return "Stab: MakePayment";
+        }
+
+        /// <summary>
+        /// Get order processing status.
+        /// </summary>
+        [HttpGet("GetOrderStatus", Name = "GetOrderStatus")]
+        public string GetOrderStatus()
+        {
+            return "Stab: GetOrderStatus";
+        }
+    }
+}
