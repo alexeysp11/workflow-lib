aaece3800fbac2a02a43beb10346444528f97644
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-14 22:46:35 +0500

    issue-99: created Executives user group

--- examples/UnifiedBusinessPlatform/sql/useraccounts.sql
+++ examples/UnifiedBusinessPlatform/sql/useraccounts.sql
@@ -31,3 +31,31 @@ SELECT
   e."BusinessEntityStatus"
 FROM "Employees" e
 JOIN "UserAccounts" ua ON e."FirstName" || '.' || e."LastName" = ua."Login";
+
+-- Executives.
+INSERT INTO "UserGroups" ("IsGroupByDefault", "IsSystem", "Uid", "Name", "Description", "DateCreated", "DateChanged", "BusinessEntityStatus")
+VALUES (FALSE, FALSE, uuid_generate_v4(), 'Executives', 'Executives', NOW(), NOW(), 1);
+
+-- Executives.
+INSERT INTO "UserAccountGroups" ("UserAccountId", "UserGroupId", "Uid", "DateCreated", "DateChanged", "BusinessEntityStatus")
+SELECT
+	ua."Id",
+	(SELECT "Id" FROM "UserGroups" WHERE "Name" = 'Executives' LIMIT 1),
+	uuid_generate_v4(),
+	NOW(),
+	NOW(),
+	1
+FROM "UserAccounts" ua
+INNER JOIN "EmployeeUserAccounts" eua ON eua."UserAccountId" = ua."Id"
+INNER JOIN "Employees" e ON e."Id" = eua."EmployeeId"
+INNER JOIN "EmployeeOrganizationItem" eoi ON eoi."EmployeesId" = e."Id"
+INNER JOIN "OrganizationItems" oi ON oi."Id" = eoi."OrganizationItemsId"
+WHERE (
+		oi."Name" LIKE '%System Administrator%'
+		OR oi."Name" LIKE '%Director%'
+		OR oi."Name" LIKE '%CEO%'
+		OR oi."Name" LIKE '%President%'
+		OR oi."Name" = 'IT Infrastructure Manager'
+	)
+	AND NOT oi."Name" LIKE '%Art Director%'
+ORDER BY oi."Name";
