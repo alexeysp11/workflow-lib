138a87c7c27683fd22f712cbaff9750167f2b749
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-10-16 10:57:00 +0500

    issue-99: created SignIn page

---
+++ examples/UnifiedBusinessPlatform/webapp/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using WorkflowLib.Examples.UnifiedBusinessPlatform.ViewModels;
+
+namespace WorkflowLib.Examples.UnifiedBusinessPlatform.Controllers;
+
+public class Auth : Controller
+{
+    public IActionResult SignIn()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignIn(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // var User = from m in _context.SignIn select m;
+            // User = User.Where(s => s.username.Contains(model.username));
+            // if (User.Count() != 0)
+            // {
+            //     if (User.First().password == model.password)
+            //     {
+            //         return RedirectToAction("Success");
+            //     }
+            // }
+        }
+        return RedirectToAction("SignIn");
+    }
+}
