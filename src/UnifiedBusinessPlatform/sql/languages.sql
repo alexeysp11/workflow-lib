0b4d8ca18da920eaa07e7dd42065f8725eacbc69
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-12-31 22:07:06 +0500

    issue-110: description for the Hiring page

--- src/UnifiedBusinessPlatform/sql/languages.sql
+++ src/UnifiedBusinessPlatform/sql/languages.sql
@@ -28,6 +28,7 @@ VALUES
     ('Employees', 'Employees', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('EmployeesDescribe', 'A complete list of company employees with the ability to view and edit personal information, positions and contact details. Managers and HR specialists can maintain and record employment contracts and personnel transfers', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Hiring', 'Hiring specialists', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
+    ('HiringDescribe', 'Post jobs, track candidates, conduct interviews, and manage the hiring process from start to finish', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Customers', 'Customers', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Organizations', 'Organizations', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('OrganizationDetails', 'Organization details', 2, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
@@ -129,6 +130,7 @@ VALUES
     ('Employees', 'Сотрудники', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('EmployeesDescribe', 'Полный список сотрудников компании с возможностью просмотра и редактирования личной информации, должностей и контактных данных. Для руководителей и HR-специалистов доступно ведение и учет трудовых договоров и кадровых перемещений', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Hiring', 'Найм специалистов', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
+    ('HiringDescribe', 'Публикуйте вакансии, отслеживайте кандидатов, проводите собеседования и управляйте процессом найма от начала до конца', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Customers', 'Потребители', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Organizations', 'Организации', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('OrganizationDetails', 'Детали организации', 3, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
@@ -230,6 +232,7 @@ VALUES
     ('Employees', 'Mitarbeiter', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('EmployeesDescribe', 'Eine vollständige Liste der Mitarbeiter des Unternehmens mit der Möglichkeit, persönliche Informationen, Positionen und Kontaktdaten anzuzeigen und zu bearbeiten. Management- und Personalfachkräfte können Arbeitsverträge und Personalbewegungen pflegen und erfassen', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Hiring', 'Spezialisten einstellen', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
+    ('HiringDescribe', 'Veröffentlichen Sie Stellenangebote, verfolgen Sie Kandidaten, führen Sie Vorstellungsgespräche und verwalten Sie den Einstellungsprozess von Anfang bis Ende', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Customers', 'Kunden', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Organizations', 'Organisationen', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('OrganizationDetails', 'Organisationsdetails', 4, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
@@ -331,6 +334,7 @@ VALUES
     ('Employees', 'Empleados', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('EmployeesDescribe', 'Una lista completa de los empleados de la empresa con la capacidad de ver y editar información personal, puestos y datos de contacto. Los especialistas en gestión y recursos humanos pueden mantener y registrar contratos laborales y movimientos de personal.', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Hiring', 'Contratar especialistas', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
+    ('HiringDescribe', 'Publique ofertas de trabajo, realice un seguimiento de los candidatos, realice entrevistas y gestione el proceso de contratación de principio a fin.', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Customers', 'Clientes', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('Organizations', 'Organizaciones', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
     ('OrganizationDetails', 'Detalles de la organización', 5, 'd7b0eb60-190e-439d-89e1-c78594ac7f0c'),
@@ -528,6 +532,7 @@ VALUES
 INSERT INTO public."LanguageKeyForms"("Key", "FormName", "ApplicationUid", "DateCreated", "DateChanged", "BusinessEntityStatus")
 VALUES 
     ('Hiring', 'Hrm/Hiring', 'd7b0eb60-190e-439d-89e1-c78594ac7f0c', now(), now(), 1);
+    ('HiringDescribe', 'Hrm/Hiring', 'd7b0eb60-190e-439d-89e1-c78594ac7f0c', now(), now(), 1);
 
 INSERT INTO public."LanguageKeyForms"("Key", "FormName", "ApplicationUid", "DateCreated", "DateChanged", "BusinessEntityStatus")
 VALUES 
