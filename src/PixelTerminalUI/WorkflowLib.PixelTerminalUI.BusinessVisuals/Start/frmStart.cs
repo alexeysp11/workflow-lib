6efe56afe8d23dda8a307f75d91cca96f0d88a1d
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-03-22 21:52:08 +0500

    issue-137: start page

--- src/PixelTerminalUI/WorkflowLib.PixelTerminalUI.BusinessVisuals/Start/frmStart.cs
+++ src/PixelTerminalUI/WorkflowLib.PixelTerminalUI.BusinessVisuals/Start/frmStart.cs
@@ -1,3 +1,4 @@
+using WorkflowLib.PixelTerminalUI.BusinessVisuals.Menu;
 using WorkflowLib.PixelTerminalUI.ServiceEngine.Controls;
 using WorkflowLib.PixelTerminalUI.ServiceEngine.Forms;
 
@@ -5,7 +6,11 @@ namespace WorkflowLib.PixelTerminalUI.BusinessVisuals.Start;
 
 public class frmStart : BaseForm
 {
-    private TextControl? lblHeader;
+    private TextControl? lblWelcome;
+    private TextControl? lblAppName;
+
+    private TextEditControl? txtUserInput;
+
     public frmStart() : base()
     {
         Name = nameof(frmStart);
@@ -13,14 +18,54 @@ public class frmStart : BaseForm
     
     protected override void InitializeComponent()
     {
-        lblHeader = new TextControl();
-        lblHeader.Name = nameof(lblHeader);
-        lblHeader.Top = 0;
-        lblHeader.Left = 0;
-        lblHeader.EntireLine = true;
-        lblHeader.HorizontalAlignment = HorizontalAlignment.Center;
-        lblHeader.Value = "PIXEL TERMINAL UI";
-        lblHeader.Inverted = true;
-        Controls.Add(lblHeader);
+        lblWelcome = new TextControl();
+        lblWelcome.Name = nameof(lblWelcome);
+        lblWelcome.Top = 4;
+        lblWelcome.Left = 0;
+        lblWelcome.EntireLine = true;
+        lblWelcome.HorizontalAlignment = HorizontalAlignment.Center;
+        lblWelcome.Value = "WELCOME TO";
+        Controls.Add(lblWelcome);
+
+        lblAppName = new TextControl();
+        lblAppName.Name = nameof(lblAppName);
+        lblAppName.Top = 5;
+        lblAppName.Left = 0;
+        lblAppName.EntireLine = true;
+        lblAppName.HorizontalAlignment = HorizontalAlignment.Center;
+        lblAppName.Value = "PIXEL TERMINAL UI";
+        Controls.Add(lblAppName);
+
+        txtUserInput = new TextEditControl();
+        txtUserInput.Name = nameof(txtUserInput);
+        txtUserInput.Top = 14;
+        txtUserInput.Left = 0;
+        txtUserInput.EntireLine = true;
+        txtUserInput.Hint = "PRESS ENTER TO CONTINUE";
+        txtUserInput.EnterValidation = txtUserInput_EnterValidation;
+        Controls.Add(txtUserInput);
+    }
+
+    private bool txtUserInput_EnterValidation()
+    {
+        try
+        {
+            switch (txtUserInput.Value)
+            {
+                case "":
+                case "-n":
+                    ShowForm(new frmMenu());
+                    break;
+
+                case "-q":
+                    ShowInformation("Are you sure to exit the application?");
+                    break;
+            }
+        }
+        finally
+        {
+            txtUserInput.Value = "";
+        }
+        return true;
     }
 }
