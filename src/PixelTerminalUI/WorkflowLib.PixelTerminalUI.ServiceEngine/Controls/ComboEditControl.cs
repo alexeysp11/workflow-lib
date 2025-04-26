5d6c54114f8a336d651aa1bec33f9fdea1a73c29
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-04-26 20:49:28 +0500

    issue-141: comments

--- src/PixelTerminalUI/WorkflowLib.PixelTerminalUI.ServiceEngine/Controls/ComboEditControl.cs
+++ src/PixelTerminalUI/WorkflowLib.PixelTerminalUI.ServiceEngine/Controls/ComboEditControl.cs
@@ -93,6 +93,10 @@ public class ComboEditControl : TextEditControl
         }
     }
 
+    /// <summary>
+    /// Validation performed during user input processing.
+    /// </summary>
+    /// <returns>true if the validation was performed correctly; otherwise false</returns>
     private bool OnComboEnterValidation()
     {
         try
