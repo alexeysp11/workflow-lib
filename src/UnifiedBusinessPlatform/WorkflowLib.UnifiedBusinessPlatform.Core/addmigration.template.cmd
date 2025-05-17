78743913beeb6b732fc5f17b730f6083ce287b12
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-05-17 23:33:06 +0500

    issue-152: update template

--- src/UnifiedBusinessPlatform/WorkflowLib.UnifiedBusinessPlatform.Core/addmigration.template.cmd
+++ src/UnifiedBusinessPlatform/WorkflowLib.UnifiedBusinessPlatform.Core/addmigration.template.cmd
@@ -1,5 +1,5 @@
 
 dotnet build
 
-dotnet ef migrations add InitialCreate --project core/WorkflowLib.UnifiedBusinessPlatform.Core.csproj --startup-project webapp/WorkflowLib.UnifiedBusinessPlatform.csproj
-dotnet ef database update --project core/WorkflowLib.UnifiedBusinessPlatform.Core.csproj --startup-project webapp/WorkflowLib.UnifiedBusinessPlatform.csproj
+dotnet ef migrations add InitialCreate --project WorkflowLib.UnifiedBusinessPlatform.Core/WorkflowLib.UnifiedBusinessPlatform.Core.csproj --startup-project WorkflowLib.UnifiedBusinessPlatform/WorkflowLib.UnifiedBusinessPlatform.csproj
+dotnet ef database update --project WorkflowLib.UnifiedBusinessPlatform.Core/WorkflowLib.UnifiedBusinessPlatform.Core.csproj --startup-project WorkflowLib.UnifiedBusinessPlatform/WorkflowLib.UnifiedBusinessPlatform.csproj
