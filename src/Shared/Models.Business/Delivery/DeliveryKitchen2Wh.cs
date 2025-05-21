965698567bc2d4c4100f781d9819fbe6c0394b8b
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-05-21 11:47:13 +0500

    issue-153: warnings

--- src/Shared/Models.Business/Delivery/DeliveryKitchen2Wh.cs
+++ src/Shared/Models.Business/Delivery/DeliveryKitchen2Wh.cs
@@ -9,6 +9,7 @@ namespace WorkflowLib.Shared.Models.Business.Delivery
     /// Model for transferring a finished order from the kitchen to the warehouse 
     /// (shipping point, destination, start time, end time, products, order number, generated order QR code).
     /// </summary>
+    [Obsolete("Should be moved to the client side code")]
     public class DeliveryKitchen2Wh : DeliveryOperation, IWfBusinessEntity
     {
         /// <summary>
--- src/Shared/Models.Business/Delivery/DeliveryWh2Kitchen.cs
+++ src/Shared/Models.Business/Delivery/DeliveryWh2Kitchen.cs
@@ -10,6 +10,7 @@ namespace WorkflowLib.Shared.Models.Business.Delivery
     /// Model for transferring products from warehouse to kitchen 
     /// (shipping point, destination, start time, end time, products, ingredients).
     /// </summary>
+    [Obsolete("Should be moved to the client side code")]
     public class DeliveryWh2Kitchen : DeliveryOperation, IWfBusinessEntity
     {
         /// <summary>
