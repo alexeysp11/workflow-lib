8b69e41e0f6effcb3bf9dbf71d64819f2af9be82
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-06-06 12:56:57 +0500

    issue-157: DbInit

---
+++ src/ECommerce/WorkflowLib.ECommerce.FoodDelivery.DbInit/IStartupInstance.cs
@@ -0,0 +1,12 @@
+namespace WorkflowLib.ECommerce.FoodDelivery.DbInit;
+
+/// <summary>
+/// Interface for standardized launch of an application instance.
+/// </summary>
+public interface IStartupInstance
+{
+    /// <summary>
+    /// Method for standardized launch of an application instance.
+    /// </summary>
+    void Start();
+}
--- src/ECommerce/WorkflowLib.ECommerce.FoodDelivery.DbInit/Program.cs
+++ src/ECommerce/WorkflowLib.ECommerce.FoodDelivery.DbInit/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace WorkflowLib.ECommerce.FoodDelivery.DbInit
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using WorkflowLib.ECommerce.FoodDelivery.DbInit.Models;
+using WorkflowLib.ECommerce.FoodDelivery.Core.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkflowLib.ECommerce.FoodDelivery.DbInit;
+
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var host = CreateHostBuilder(args).Build();
+        var app = host.Services.GetRequiredService<IStartupInstance>();
+        app.Start();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                ConfigureServices(services);
+            });
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        // Register application instance.
+        services.AddSingleton<IStartupInstance, StartupInstance>();
+
+        // Configuration settings.
+        var appsettingsConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var appsettings = appsettingsConfig.GetSection("DbInitSettings").Get<DbInitSettings>();
+        if (string.IsNullOrEmpty(appsettings.ConnectionString))
         {
-            Console.WriteLine("Hello, World!");
+            throw new Exception($"Cannot start the application: connection string is not initialized");
         }
+        services.AddSingleton(appsettings);
+        services.AddDbContext<FoodDeliveryDbContext>(options => options.UseNpgsql(appsettings.ConnectionString));
     }
 }
