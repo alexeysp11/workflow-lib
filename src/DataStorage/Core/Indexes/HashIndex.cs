21340f6e989c0297ac5b9a6cbe3d9a5e3892fc26
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-08-20 23:30:44 +0500

    issue-184: InMemoryHashTable

--- src/DataStorage/Core/Indexes/HashIndex.cs
+++ src/DataStorage/Core/Indexes/HashIndex.cs
@@ -1,57 +1,10 @@
+using WorkflowLib.DataStorage.Core.Tables;
+
 namespace WorkflowLib.DataStorage.Core.Indexes;
 
 /// <summary>
 /// The HashIndex class, which implements a hash index.
 /// </summary>
-public class HashIndex<TKey, TValue>
+public class HashIndex<TKey, TValue> : InMemoryHashTable<TKey, TValue>
 {
-    private Dictionary<TKey, TValue> _index;
-
-    /// <summary>
-    /// Default constructor.
-    /// </summary>
-    public HashIndex()
-    {
-        _index = new Dictionary<TKey, TValue>();
-    }
-
-    /// <summary>
-    /// Add an element to the index.
-    /// </summary>
-    public void AddElement(TKey key, TValue value)
-    {
-        if (!_index.ContainsKey(key))
-        {
-            _index.Add(key, value);
-        }
-        else
-        {
-            _index[key] = value;
-        }
-    }
-
-    /// <summary>
-    /// Remove an element from the index.
-    /// </summary>
-    public bool RemoveElement(TKey key)
-    {
-        if (_index.ContainsKey(key))
-        {
-            _index.Remove(key);
-            return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Search for an element in the index.
-    /// </summary>
-    public TValue? SearchElement(TKey key)
-    {
-        if (_index.ContainsKey(key))
-        {
-            return _index[key];
-        }
-        return default(TValue);
-    }
 }
--- src/DataStorage/Core/Tables/InMemoryHashTable.cs
+++ src/DataStorage/Core/Tables/InMemoryHashTable.cs
@@ -1,7 +1,54 @@
-﻿using WorkflowLib.DataStorage.Core.Indexes;
+﻿namespace WorkflowLib.DataStorage.Core.Tables;
 
-namespace WorkflowLib.DataStorage.Core.Tables;
-
-public class InMemoryHashTable<TKey, TValue> : HashIndex<TKey, TValue>
+public class InMemoryHashTable<TKey, TValue>
 {
+    private Dictionary<TKey, TValue> _records;
+
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public InMemoryHashTable()
+    {
+        _records = new Dictionary<TKey, TValue>();
+    }
+
+    /// <summary>
+    /// Add an element to the hash table.
+    /// </summary>
+    public void AddElement(TKey key, TValue value)
+    {
+        if (!_records.ContainsKey(key))
+        {
+            _records.Add(key, value);
+        }
+        else
+        {
+            _records[key] = value;
+        }
+    }
+
+    /// <summary>
+    /// Remove an element from the hash table.
+    /// </summary>
+    public bool RemoveElement(TKey key)
+    {
+        if (_records.ContainsKey(key))
+        {
+            _records.Remove(key);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Search for an element in the hash table.
+    /// </summary>
+    public TValue? SearchElement(TKey key)
+    {
+        if (_records.ContainsKey(key))
+        {
+            return _records[key];
+        }
+        return default(TValue);
+    }
 }
