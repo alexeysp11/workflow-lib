7468eedc4303024a2742cd01e51d30aa9bdf2902
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-08-22 13:36:56 +0500

    issue-185: InMemoryStorageService

--- src/DataStorage/WorkflowLib.DataStorage.InMemoryService.Grpc/Services/InMemoryStorageService.cs
+++ src/DataStorage/WorkflowLib.DataStorage.InMemoryService.Grpc/Services/InMemoryStorageService.cs
@@ -1,38 +1,41 @@
 using Grpc.Core;
+using System.Threading.Tasks;
+using WorkflowLib.DataStorage.Core.Tables;
 using WorkflowLib.DataStorage.InMemoryService.Grpc;
 
 namespace WorkflowLib.DataStorage.InMemoryService.Grpc.Services;
 
 public class InMemoryStorageService : InMemoryStorage.InMemoryStorageBase
 {
+    private readonly InMemoryHashTable<string, string> _hashTable;
     private readonly ILogger<InMemoryStorageService> _logger;
 
     public InMemoryStorageService(ILogger<InMemoryStorageService> logger)
     {
+        _hashTable = new InMemoryHashTable<string, string>();
         _logger = logger;
     }
 
-    public override Task<InMemoryReply> Save(SaveRequest request, ServerCallContext context)
+    public override Task<SaveResponse> Save(SaveRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new InMemoryReply
-        {
-            Message = $"Key: {request.Key}, value: {request.Value}"
-        });
+        _logger.LogInformation($"Save called with Key: {request.Key}, Value: {request.Value}");
+        _hashTable.AddElement(request.Key, request.Value);
+        return Task.FromResult(new SaveResponse { Success = true });
     }
 
-    public override Task<InMemoryReply> Search(SearchRequest request, ServerCallContext context)
+    public override Task<SearchResponse> Search(SearchRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new InMemoryReply
-        {
-            Message = $"Key: {request.Key}"
-        });
+        _logger.LogInformation($"Search called with Key: {request.Key}");
+        string? value = _hashTable.SearchElement(request.Key);
+        bool found = value != null;
+
+        return Task.FromResult(new SearchResponse { Value = value ?? "", Found = found });
     }
 
-    public override Task<InMemoryReply> Remove(RemoveRequest request, ServerCallContext context)
+    public override Task<RemoveResponse> Remove(RemoveRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new InMemoryReply
-        {
-            Message = $"Key: {request.Key}"
-        });
+        _logger.LogInformation($"Remove called with Key: {request.Key}");
+        bool success = _hashTable.RemoveElement(request.Key);
+        return Task.FromResult(new RemoveResponse { Success = success });
     }
 }
