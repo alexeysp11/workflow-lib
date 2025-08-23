ee674a5c27bdf1e5086e06af54862f2acd68e5d2
alexeysp11 <alexeyspiridon1111@gmail.com>
2025-08-23 15:29:23 +0500

    issue-185: reduce string allocation if key is null or empty

--- src/DataStorage/WorkflowLib.DataStorage.InMemoryService.Grpc/Services/InMemoryStorageService.cs
+++ src/DataStorage/WorkflowLib.DataStorage.InMemoryService.Grpc/Services/InMemoryStorageService.cs
@@ -18,22 +18,12 @@ public class InMemoryStorageService : InMemoryStorage.InMemoryStorageBase
     public override Task<SaveResponse> Save(SaveRequest request, ServerCallContext context)
     {
         string requestUid = Guid.NewGuid().ToString();
+        CheckIfRequestKeyIsNullOrEmpty(requestUid, request.Key);
         try
         {
-            if (string.IsNullOrEmpty(request.Key))
-            {
-                throw new ArgumentNullException(nameof(request.Key));
-            }
-
             Log.Information($"[UID: {requestUid}] Save called with Key: '{request.Key}', Value: '{request.Value}'");
             _hashTable.AddElement(request.Key, request.Value);
             return Task.FromResult(new SaveResponse { Success = true });
-        }
-        catch (ArgumentNullException ex)
-        {
-            string errorMessage = $"[UID: {requestUid}] Key cannot be null";
-            Log.Error(ex, errorMessage);
-            throw new RpcException(new Status(StatusCode.InvalidArgument, errorMessage), ex.Message);
         }
         catch (Exception ex)
         {
@@ -46,24 +36,14 @@ public class InMemoryStorageService : InMemoryStorage.InMemoryStorageBase
     public override Task<SearchResponse> Search(SearchRequest request, ServerCallContext context)
     {
         string requestUid = Guid.NewGuid().ToString();
+        CheckIfRequestKeyIsNullOrEmpty(requestUid, request.Key);
         try
         {
-            if (string.IsNullOrEmpty(request.Key))
-            {
-                throw new ArgumentNullException(nameof(request.Key));
-            }
-
             Log.Information($"[UID: {requestUid}] Search called with Key: '{request.Key}'");
             string? value = _hashTable.SearchElement(request.Key);
             bool found = value != null;
 
             return Task.FromResult(new SearchResponse { Value = value ?? "", Found = found });
-        }
-        catch (ArgumentNullException ex)
-        {
-            string errorMessage = $"[UID: {requestUid}] Key cannot be null";
-            Log.Error(ex, errorMessage);
-            throw new RpcException(new Status(StatusCode.InvalidArgument, errorMessage), ex.Message);
         }
         catch (Exception ex)
         {
@@ -76,28 +56,28 @@ public class InMemoryStorageService : InMemoryStorage.InMemoryStorageBase
     public override Task<RemoveResponse> Remove(RemoveRequest request, ServerCallContext context)
     {
         string requestUid = Guid.NewGuid().ToString();
+        CheckIfRequestKeyIsNullOrEmpty(requestUid, request.Key);
         try
         {
-            if (string.IsNullOrEmpty(request.Key))
-            {
-                throw new ArgumentNullException(nameof(request.Key));
-            }
-
             Log.Information($"[UID: {requestUid}] Remove called with Key: '{request.Key}'");
             bool success = _hashTable.RemoveElement(request.Key);
             return Task.FromResult(new RemoveResponse { Success = success });
-        }
-        catch (ArgumentNullException ex)
-        {
-            string errorMessage = $"[UID: {requestUid}] Key cannot be null";
-            Log.Error(ex, errorMessage);
-            throw new RpcException(new Status(StatusCode.InvalidArgument, errorMessage), ex.Message);
         }
         catch (Exception ex)
         {
             string errorMessage = $"[UID: {requestUid}] An unexpected error occurred while removing element with Key: '{request.Key}'";
             Log.Error(ex, errorMessage);
             throw new RpcException(new Status(StatusCode.Internal, errorMessage), ex.Message);
+        }
+    }
+
+    private void CheckIfRequestKeyIsNullOrEmpty(string requestUid, string requestKey)
+    {
+        if (string.IsNullOrEmpty(requestKey))
+        {
+            string errorMessage = $"[UID: {requestUid}] Key cannot be null";
+            Log.Error(errorMessage);
+            throw new RpcException(new Status(StatusCode.InvalidArgument, errorMessage));
         }
     }
 }
